d5cc79f1390715b2528ccfe9cc7afe8b597a7a7b
Anthony Hadwan <anthony.hadwan@hec.edu>
2021-01-27 19:24:03 +0200

    Worked on the Index, New and create methods

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -2,13 +2,20 @@ class InvestmentsController < ApplicationController
 
   def index
     @investments = current_user.investments
-
   end
 
   def new
+    @investment = Investment.new
   end
 
   def create
+    @investment = Investment.new(params[:investment])
+    @investment.user = current_user
+    if @ginvestment.save
+      redirect_to investments_path, notice: "Transaction Successfully added!"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,4 +26,5 @@ class InvestmentsController < ApplicationController
 
   def destroy
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Clearing DB"
+User.destroy_all
+Coin.destroy_all
+Investment.destroy_all
+puts"done!"
+
+puts"Seeding!"
 Anthony = User.create(name: "Anthony", email: "anthony@crypto.com", password: "secret")
 
 Bitcoin = Coin.create(name: "Bitcoin", open: 34312.34, close: 30127.34, high: 35000, low: 29300, volume: 200000000, market_cap: 503000000)
 
-Investment_1 = Investment.create(user_id: 1, coin_id: 1, coin_price: 22000, quantity: 0.001234, coin_name: "Bitcoin")
+Investment_1 = Investment.create(user_id: 3, coin_id: 6, coin_price: 22000, quantity: 0.001234, coin_name: "Bitcoin")
+puts "done!"
