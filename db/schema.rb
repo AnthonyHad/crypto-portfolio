9b79dec9a8ffd385c793c386ef9a7f69dcbfc42d
Anthony Hadwan <anthony.hadwan@hec.edu>
2021-01-27 17:07:22 +0200

    removed irrelevant columns from the coins able and added a seed for 1 coin

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_144021) do
+ActiveRecord::Schema.define(version: 2021_01_27_150254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "coins", force: :cascade do |t|
     t.string "name"
-    t.float "latest_price"
-    t.float "last_24h"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.float "open"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Anthony = User.create(name: "Anthony", email: "anthony@crypto.com", password: "secret")
+
+Bitcoin = Coin.create(name: "Bitcoin", open: 34312.34, close: 30127.34, high: 35000, low: 29300, volume: 200000000, market_cap: 503000000)
