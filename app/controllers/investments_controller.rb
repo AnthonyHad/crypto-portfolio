64dca3617be9aeb12d212d78ba834d302cb4e93e
Anthony Hadwan <anthony.hadwan@hec.edu>
2021-02-16 17:57:26 +0200

    Investment Aggregation code refactoring

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -2,6 +2,7 @@ class InvestmentsController < ApplicationController
 
   def index
     @investments = current_user.investments
+    investment_aggregation
   end
 
   def new
@@ -33,6 +34,28 @@ class InvestmentsController < ApplicationController
     params.require(:investment).permit(:coin_id, :coin_price, :quantity)
   end
 
+  def investment_aggregation
+    coin_names = []
+    @user_investments = {}
+    @investments.each do |investement|
+      coin_names << investement.coin.name
+    end
+    s_names =  coin_names.uniq
 
+    s_names.each do |name|
+      @user_investments[name] = {}
+    end
+    @user_investments.each do |key, value|
+      value[:price] = []
+      value[:quantity]= []
+       @investments.each do |investment|
+          if investment.coin.name == key
+            value[:price]<< investment.coin_price
+            value[:quantity] << investment.quantity
+          end
+        end
+      end
+  return @user_investments
+  end
 end
 
