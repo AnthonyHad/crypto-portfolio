9cbf0daa2b30c6e0b1e73c73368ab1113fd1e311
Anthony Hadwan <anthony.hadwan@hec.edu>
2021-03-16 22:15:35 +0100

    Added coin name to appear when a user wants to check investments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'users#show' # 'pages#home'
+  root to: 'coins#index' # 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   resources :coins, only: [:index, :new, :create]
