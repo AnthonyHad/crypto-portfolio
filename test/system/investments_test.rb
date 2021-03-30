f252df0ee29b9709b819d38affd6bf3fbaed70c9
Anthony Hadwan <anthony.hadwan@hec.edu>
2021-03-30 19:01:48 +0200

    meddled abit with testing

---
+++ test/system/investments_test.rb
@@ -0,0 +1,22 @@
+require "application_system_test_case"
+
+class InvestmentsTest < ApplicationSystemTestCase
+  test "visiting the  Investment index" do
+    login_as users(:anthony)
+
+    visit investments_url
+
+    click_on '+Add a transaction'
+    within("select2-search__field") do
+      fill_in 'Coin', with: 'Bitcoin'
+    end
+    save_and_open_screenshot
+    fill_in 'Coin price', with: '10000'
+    fill_in 'Quantity', with: '2'
+    click_on 'Add Transaction'
+
+
+    assert_selector "h3", text: "Your Crypto Assets"
+
+  end
+end
