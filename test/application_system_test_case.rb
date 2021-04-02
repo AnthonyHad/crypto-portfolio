a5e67d2e1e3d1cf1ad008f5dd2955bc2bd4d573e
Anthony Hadwan <anthony.hadwan@hec.edu>
2021-04-02 12:04:28 +0200

    Improved test for adding an investment

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,7 @@ require "test_helper"
 Webdrivers::Chromedriver.update
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include CapybaraSelect2
+  include CapybaraSelect2::Helpers
   driven_by :headless_chrome
 end
--- test/system/investments_test.rb
+++ test/system/investments_test.rb
@@ -1,22 +1,24 @@
 require "application_system_test_case"
 
 class InvestmentsTest < ApplicationSystemTestCase
-  test "visiting the  Investment index" do
+
+  setup do
+    @coin = coins(:Bitcoin)
+  end
+
+  test "can create a new investment" do
     login_as users(:anthony)
+
 
     visit investments_url
 
     click_on '+Add a transaction'
-    within("select2-search__field") do
-      fill_in 'Coin', with: 'Bitcoin'
-    end
-    save_and_open_screenshot
+    select2_open label: 'Coin Name'
+    select2_search 'Bitcoin', from: 'Coin Name'
+    select2_select 'Bitcoin', from: 'Coin Name'
     fill_in 'Coin price', with: '10000'
     fill_in 'Quantity', with: '2'
     click_on 'Add Transaction'
-
-
-    assert_selector "h3", text: "Your Crypto Assets🪙"
-
+    assert_selector "th", text: "Bitcoin BTC"
   end
 end
