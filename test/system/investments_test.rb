a48cf48f2636eabe0c5d4b98c5f536691f1eb6c5
Anthony Hadwan <anthony.hadwan@hec.edu>
2021-04-02 12:34:16 +0200

    added tests for investments crud actions

--- test/system/investments_test.rb
+++ test/system/investments_test.rb
@@ -21,4 +21,41 @@ class InvestmentsTest < ApplicationSystemTestCase
     click_on 'Add Transaction'
     assert_selector "th", text: "Bitcoin BTC"
   end
+
+
+  test "can create edit a new investment" do
+    login_as users(:anthony)
+    visit investments_url
+    click_on '+Add a transaction'
+    select2_open label: 'Coin Name'
+    select2_search 'Bitcoin', from: 'Coin Name'
+    select2_select 'Bitcoin', from: 'Coin Name'
+    fill_in 'Coin price', with: '10000'
+    fill_in 'Quantity', with: '2'
+    click_on 'Add Transaction'
+    find('.fa-exchange-alt').click
+    find('.fa-edit ').click
+    fill_in 'Coin price', with: '30000'
+    fill_in 'Quantity', with: '3'
+    click_on 'Add Transaction'
+    assert_selector "td", text: "30,000.00$"
+  end
+
+  test "can  delete an investment" do
+    login_as users(:anthony)
+    visit investments_url
+    click_on '+Add a transaction'
+    select2_open label: 'Coin Name'
+    select2_search 'Bitcoin', from: 'Coin Name'
+    select2_select 'Bitcoin', from: 'Coin Name'
+    fill_in 'Coin price', with: '10000'
+    fill_in 'Quantity', with: '2'
+    click_on 'Add Transaction'
+    find('.fa-exchange-alt').click
+    find('.fa-trash-alt ').click
+    page.accept_alert
+    assert_selector "h1", text: "No cryptoasset yet ! What are you waiting for to invest? 🦾"
+  end
+
+
 end
