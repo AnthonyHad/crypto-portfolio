b520c1fbdab7f59c347eb3bef5c33046b76d8666
Anthony Hadwan <anthony.hadwan@hec.edu>
2021-04-03 17:50:51 +0200

    added investment data update to be scheduled by Heroku

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,4 +1,27 @@
+require 'json'
+require 'open-uri'
+
 class Investment < ApplicationRecord
   belongs_to :user
   belongs_to :coin
+
+
+
+  def self.update_coin_data
+  investments = Investment.all
+  investments.each do |investment|
+    url = "https://api.coinpaprika.com/v1/coins/#{investment.coin_id}/ohlcv/latest/"
+    serialized = URI.open(url).read
+    parsed = JSON.parse(serialized)
+
+    investment.coin.update(
+      open: parsed[0]["open"],
+      close: parsed[0]["close"],
+      high: parsed[0]["high"],
+      low: parsed[0]["low"],
+      volume: parsed[0]["volume"],
+      market_cap: parsed[0]["market_cap"]
+      )
+    end
+   end
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,6 +1,14 @@
 desc "This task is called by the Heroku scheduler add-on"
+
 task :update_top_10 => :environment do
   puts "Updating top 10 coins..."
   Coin.update_top_10_market_data
+  puts "done."
+end
+
+
+ask :update_user_investments => :environment do
+  puts "Updating Investments..."
+  Investment.update_coin_data
   puts "done."
 end
